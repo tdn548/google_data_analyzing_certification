c15e2bc5487a9f919a774bf9c64f82a3668d3a42
tdn548 <53711944+tdn548@users.noreply.github.com>
2021-04-28 19:01:56 -0400

    Update 1.R

--- 1.R
+++ 1.R
@@ -305,6 +305,5 @@ all_trips_v2 %>%
 counts <- aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual + all_trips_v2$day_of_week, FUN = mean)
 write.csv(counts, file = 'E:/digital marketing/r/google_data_project1.csv')
 
-#You're done! Congratulations!
 
 
